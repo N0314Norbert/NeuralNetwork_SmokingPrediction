ec9f3259fdd3a08a68b90185283cc22eb99b7c38
NobertNagy <nagynorbert314@gmail.com>
2022-05-27 00:23:17 +0200

    formatting

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ class NeuralNetwork:
         self.inputs = inputs
         self.hidden = hidden
         self.outputs = outputs
-        self.learningRate = 0.0001
+        self.learningRate = 0.001
 
         self.weights_ih = np.random.uniform(-0.75, 0.75, (self.hidden, self.inputs))
         self.weights_oh = np.random.uniform(-0.75, 0.75, (self.outputs, self.hidden))
 
-        self.bias_h = np.random.uniform(-0.7, 0.7, (self.hidden, 1))
-        self.bias_o = np.random.uniform(-0.7, 0.7, (self.outputs, 1))
+        self.bias_h = np.random.uniform(-0.5, 0.5, (self.hidden, 1))
+        self.bias_o = np.random.uniform(-0.5, 0.5, (self.outputs, 1))
 
     def dsigmoid(self, num):
         return num * (num - 1)
 
     def sigmoid(self, num):
-        z = np.exp(-num)
+        z = np.exp(-1 * num)
         return 1 / (1 + z)
 
     def relu(self, input):
@@ -37,24 +37,16 @@ class NeuralNetwork:
         biasResult = np.add(hiddenResult, self.bias_h)
 
 
-        index = 0
-        for data in biasResult:
-          indexInner = 0
-          for subData in data:
-              biasResult[index, indexInner] = self.relu(biasResult[index, indexInner])
-              indexInner += 1
-          index+=1
+        for data in range(len(biasResult)):
+          for subData in range(len(biasResult[data])):
+              biasResult[data, subData] = self.relu(biasResult[data, subData])
 
         output = np.dot(self.weights_oh, biasResult)
         np.add(output, self.bias_o)
 
-        index = 0
-        for data in output:
-            indexInner = 0
-            for subData in data:
-                output[index, indexInner] = self.sigmoid(output[index, indexInner])
-                indexInner += 1
-            index += 1
+        for data in range(len(output)):
+            for subData in range(len(output[data])):
+                output[data, subData] = self.sigmoid(output[data, subData])
 
         return output
 
@@ -66,66 +58,44 @@ class NeuralNetwork:
         who_t = np.transpose(self.weights_oh)
         hidden = np.dot(who_t, trainingOutputError)
 
-        index = 0
-        for data in trainingOutput:
-            indexInner = 0
-            for subData in data:
-                trainingOutput[index, indexInner] = self.dsigmoid(trainingOutput[index, indexInner])
-                indexInner += 1
-            index += 1
+        for data in range(len(trainingOutput)):
+            for subData in range(len(trainingOutput[data])):
+                trainingOutput[data, subData] = self.dsigmoid(trainingOutput[data, subData])
 
         trainingOutputChain = np.multiply(trainingOutput, trainingOutputError)
 
-        index = 0
-        for data in trainingOutputChain:
-            indexInner = 0
-            for subData in data:
-                trainingOutputChain[index, indexInner] = trainingOutputChain[index, indexInner] * self.learningRate
-                indexInner += 1
-            index += 1
+        for data in range(len(trainingOutputChain)):
+            for subData in range(len(trainingOutputChain[data])):
+                trainingOutputChain[data, subData] = trainingOutputChain[data, subData] * self.learningRate
 
         np.add(self.bias_o, trainingOutputChain)
         trainingHidden = np.transpose(hidden)
         who_t_deltas = np.dot(trainingOutputChain, trainingHidden)
 
-        index = 0
-        for data in who_t_deltas:
-            indexInner = 0
-            for subData in data:
-                self.weights_oh[index, indexInner] = who_t_deltas[index, indexInner] + self.weights_oh[index, indexInner]
-                indexInner += 1
-            index += 1
+        for data in range(len(who_t_deltas)):
+            for subData in range(len(who_t_deltas[data])):
+                self.weights_oh[data, subData] = who_t_deltas[data, subData] + self.weights_oh[data, subData]
 
         hiddenGradient = hidden
 
-        index = 0
-        for data in hidden:
-            indexInner = 0
-            for subData in data:
-                hiddenGradient[index, indexInner] = self.drelu(hidden[index, indexInner])
-                indexInner += 1
-            index += 1
+        for data in range(len(hidden)):
+            for subData in range(len(hidden[data])):
+                hiddenGradient[data, subData] = self.drelu(hidden[data, subData])
 
         hiddenErrors = np.dot(who_t, trainingOutputError)
         hiddenGradient = np.multiply(hiddenErrors, hiddenGradient)
 
-        index = 0
-        for data in hiddenGradient:
-            indexInner = 0
-            for subData in data:
-                hiddenGradient[index, indexInner] = hiddenGradient[index, indexInner] * self.learningRate
-                indexInner += 1
-            index += 1
+        for data in range(len(hiddenGradient)):
+            for subData in range(len(hiddenGradient[data])):
+                hiddenGradient[data, subData] = hiddenGradient[data, subData] * self.learningRate
 
         np.add(self.bias_h, hiddenGradient)
         trainingInputs = np.transpose(input)
         whi_t_deltas = np.dot(hiddenGradient, trainingInputs)
 
-        index = 0
-        for data in whi_t_deltas:
-
-            self.weights_ih[index] = whi_t_deltas[index] + self.weights_ih[index]
-            index += 1
+        for data in range(len(whi_t_deltas)):
+            for subData in range(len(self.weights_ih[data])):
+                self.weights_ih[data, subData] = whi_t_deltas[data] + self.weights_ih[data, subData]
 
 
 def readData():
@@ -148,12 +118,12 @@ if __name__ == '__main__':
     df.replace(to_replace='Y', value=1, inplace=True)
     df.replace(to_replace='N', value=0, inplace=True)
 
-    for trainingData in range(2000):
+    for trainingData in range(5000):
         trainingIndex = random.randint(0, df.shape[0] - 1)
         ann.train(df.loc[trainingIndex], dataSet['smoking'].loc[trainingIndex])
 
 
-    print(np.average(ann.feedForward(df.loc[6])))
+    print(np.average(ann.feedForward(df.loc[2])))
 
 
 
